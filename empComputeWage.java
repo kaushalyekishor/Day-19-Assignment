a5a55ddbc9f892d6385298c82bbd06ad0fdb247b
Kishor <kaushalyekishor1232gmail.com>
2020-07-12 18:25:46 +0530

    Add part time code added

--- empComputeWage.java
+++ empComputeWage.java
@@ -1,7 +1,6 @@
 
 
-
-//emp Attendance code
+//emp Present Absent code
 public class empPresentAbsent
 {
         public static void main(String[] args)
@@ -22,14 +21,14 @@ public class empPresentAbsent
 
 
 
-
-// emp daily wage code
-public class dailyEmpWage
+// Add part time emp wage
+public class partTimeEMpWage
 {
         public static void main(String[] args)
         {
                 //CONSTANTS
-                int IS_FULL_TIME=1;
+                int IS_PART_TIME=1;
+                int IS_FULL_TIME=2;
                 int EMP_RATE_PER_HOUR=20;
 
                 //vARIABLES
@@ -37,17 +36,20 @@ public class dailyEmpWage
                 int empWage=0;
 
                 //computation
-                double empCheck=Math.floor(Math.random() * 10 ) % 2;
-                if(empCheck==IS_FULL_TIME)
+                double empCheck=Math.floor(Math.random() * 10 ) % 3;
+                if(empCheck==IS_PART_TIME)
+                {
+                        empHrs=4;
+                }
+                else if(empCheck==IS_FULL_TIME)
                 {
                         empHrs=8;
                 }
-                else
-                {
-                        empHrs=0;
-                }
+		else
+		{
+			empHrs=0;
+		}
         empWage=empHrs*EMP_RATE_PER_HOUR;
         System.out.println("empWage: " +empWage);
         }
 }
-
