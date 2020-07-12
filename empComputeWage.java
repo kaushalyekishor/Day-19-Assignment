569fecc990d129bd20ae5341e9b87fc227145efb
Kishor <kaushalyekishor1232gmail.com>
2020-07-12 18:31:30 +0530

    empDaily wage code

--- empComputeWage.java
+++ empComputeWage.java
@@ -20,6 +20,35 @@ public class empPresentAbsent
 }
 
 
+//emp Daily Wage code
+public class dailyEmpWage
+{
+        public static void main(String[] args)
+        {
+                //CONSTANTS
+                int IS_FULL_TIME=1;
+                int EMP_RATE_PER_HOUR=20;
+
+                //vARIABLES
+                int empHrs=0;
+                int empWage=0;
+
+                //computation
+                double empCheck=Math.floor(Math.random() * 10 ) % 2;
+                if(empCheck==IS_FULL_TIME)
+                {
+                        empHrs=8;
+                }
+                else
+                {
+                        empHrs=0;
+                }
+        empWage=empHrs*EMP_RATE_PER_HOUR;
+        System.out.println("empWage: " +empWage);
+        }
+}
+
+
 
 // Add part time emp wage
 public class partTimeEMpWage
