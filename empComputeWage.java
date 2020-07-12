51b9b0e50019ecc04b04eb7ad4f7c141e810bbc5
Kishor <kaushalyekishor1232gmail.com>
2020-07-12 18:54:32 +0530

    empWageFOr month code added

--- empComputeWage.java
+++ empComputeWage.java
@@ -20,7 +20,6 @@ public class empPresentAbsent
 }
 
 
-//emp Daily Wage code
 public class dailyEmpWage
 {
         public static void main(String[] args)
@@ -86,38 +85,40 @@ public class partTimeEMpWage
 
 
 
-//emp partFull time using switch code
-public class partFullTimeSwitch
+//emp Wage for month salary code
+public class empWageForMonthSalary
 {
-//CONSTANTS
-public static final int IS_PART_TIME=1;
-public static final int IS_FULL_TIME=2;
-public static final int EMP_RATE_PER_HOUR=20;
+       public static final int IS_PART_TIME=1;
+       public static final int IS_FULL_TIME=2;
+       public static final int empRatePerHours=20;
+       public static final int numberOfDays=20;
 
-        public static void main(String[] args)
-        {
-                //vARIABLES
-                int empHrs=0;
-                int empWage=0;
+	public static void main(String[] args)
+	{
+	   int totalSalary=0;
+	   int empHrs=0;
+	   int salary=0;
 
-                //computation
-                int empCheck=(int) Math.floor(Math.random() * 10 ) % 3;
-                switch (empCheck)
-                {
-                   case IS_PART_TIME:
-                        empHrs=4;
-			break;
-		   case IS_FULL_TIME:
-			empHrs=8;
-			break;
-		   default:
-			empHrs=0;
-                }
-        	empWage=empHrs*EMP_RATE_PER_HOUR;
-        	System.out.println("empWage: " +empWage);
-        }
+	   for(int day=1;day<=numberOfDays;day++)
+	   {
+	       double empCheck=Math.floor(Math.random()*10)%3;
+	       switch((int)empCheck)
+	       {
+		  case IS_PART_TIME:
+		  empHrs=8;
+		  break;
+
+		  case IS_FULL_TIME:
+		  empHrs=4;
+		  break;
+
+		  default:
+		  empHrs=0;
+	       }
+	   salary=empHrs*empRatePerHours;
+	   totalSalary=totalSalary+salary;
+	   }
+      System.out.println("Total Salary: "+totalSalary);
+     }
 }
-
-
-
 
